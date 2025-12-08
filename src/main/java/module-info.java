6fadeb39657a61d7809fa530c97e1c0f6908f465
Toufiq Hasan <hasantoufiq777@gmail.com>
2025-12-09 02:12:11 +0600

    login scene built sucessfully

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,16 +1,27 @@
-module com.example.coachteam {
+//module com.example.coachesteam {
+//    requires javafx.controls;
+//    requires javafx.fxml;
+//    requires javafx.web;
+//
+//    requires org.controlsfx.controls;
+//    requires com.dlsc.formsfx;
+//    requires net.synedra.validatorfx;
+//    requires org.kordamp.ikonli.javafx;
+//    requires org.kordamp.bootstrapfx.core;
+//    requires eu.hansolo.tilesfx;
+//    requires com.almasb.fxgl.all;
+//
+//    opens com.example.coachesteam to javafx.fxml;
+//    exports com.example.coachesteam;
+//
+//}
+module com.example.coachsquadorganizer {
     requires javafx.controls;
     requires javafx.fxml;
-    requires javafx.web;
 
-    requires org.controlsfx.controls;
-    requires com.dlsc.formsfx;
-    requires net.synedra.validatorfx;
-    requires org.kordamp.ikonli.javafx;
-    requires org.kordamp.bootstrapfx.core;
-    requires eu.hansolo.tilesfx;
-    requires com.almasb.fxgl.all;
+    opens com.example.coachteam.controller to javafx.fxml;
+    opens com.example.coachteam to javafx.base;
 
-    opens com.example.coachteam to javafx.fxml;
+    exports com.example.coachteam.controller;
     exports com.example.coachteam;
 }
