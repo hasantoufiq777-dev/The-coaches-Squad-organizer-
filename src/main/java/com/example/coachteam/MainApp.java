08cec64fc6b250db018bae56b00023a7d9283d6b
Toufiq Hasan <hasantoufiq777@gmail.com>
2025-12-09 01:33:19 +0600

    main updated

--- src/main/java/com/example/coachteam/MainApp.java
+++ src/main/java/com/example/coachteam/MainApp.java
@@ -5,15 +5,19 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-import java.io.IOException;
-
 public class MainApp extends Application {
+
     @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(MainApp.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
-        stage.setScene(scene);
+    public void start(Stage stage) throws Exception {
+        FXMLLoader fxmlLoader = new FXMLLoader(
+                MainApp.class.getResource("/com/example/coachteam/login-view.fxml")
+        );
+        stage.setTitle("Coach Squad Organizer");
+        stage.setScene(new Scene(fxmlLoader.load()));
         stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
     }
 }
